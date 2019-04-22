a9c7fac7317b8a7961f499f0b37ea0a20520f3e4
jacktrinh97 <jacktrinh194@gmail.com>
2019-04-22 23:59:00 +0700

    Create edit profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+  devise_for :users,
+  	controller: {registrations: 'registrations'}
   
   resources :users, only: [:show]
 end
