944731be232dabe994b1426b33ac6da810d6c8a3
jacktrinh97 <jacktrinh194@gmail.com>
2019-04-23 20:11:04 +0700

    avatar_url helper and fix avatar on show, edit profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
-	# def avatar_url user
-	# 	return user.image.gsub!("_normal", "") if user.image
-	# 	gavatar_id = Digest::MD5::hexdigest(user.email).downcase
-	# 	"https://www.gavatar.com/avatar/#{gavatar_id}.jpg"
-	# end
+	def avatar_url user
+	    return "http://graph.facebook.com/#{user.uid}/picture?type=large" if user.image
+	    "user.png"
+	 end
 end
